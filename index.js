8cfc00559b5ab46b9d042067a45a3e48e01b7b45
Janée Artis <terri.artis@du.edu>
2021-04-28 22:01:12 -0600

    Getting html to render correctly and js fucntioning properly

--- index.js
+++ index.js
@@ -1,3 +1,93 @@
 const inquirer = require("inquirer");
+const fs = require("fs")
+const htmlRender = require("./src/htmlTemplate");
+const Engineer = require("./lib/engineer")
+const Intern = require("./lib/intern")
+const Manager = require("./lib/manager")
 
+const employees = [];
 
+function start (){
+
+    inquirer.prompt([
+        {
+            message: "Select a team member's role to add to the profile:",
+            name: "role",
+            type: "list",
+            choices: ["Engineer", "Manager", "Intern", "End"]
+        },
+    ]).then(choiceres => {
+        if(choiceres.role == "End"){
+            fs.writeFile('./output/test.html', htmlRender(arr), err=>{
+                if (err) throw err;
+            });
+        }else {
+            askBasic(choiceres)
+        }
+    })
+}
+
+function askBasic(param){
+    inquirer.prompt([
+        {
+            message: "Enter team member's name:",
+            type: "input",
+            name: "name"
+        },
+        {
+            message: "Enter team member's id:",
+            type: "input",
+            name: "id"
+        },
+        {
+            message: "Enter team member's email address:",
+            type: "input",
+            name: "email",
+        }
+    ]).then(res => {
+        if(param.role == "Engineer"){
+            inquirer.prompt([ 
+                {
+                message: "Enter team member's GitHub:",
+                type: "input",
+                name: "github"
+                }
+            ]).then(engineerRes => {
+                let engineer = new Engineer(param.role, res.name, res.id, res.email, engineerRes.github)
+                arr.push(engineer);
+                console.log(arr)
+                start()
+            })
+        }
+        else if(param.role == "Intern") {
+            inquirer.prompt([
+                {
+                    message: "Enter team member's school name:",
+                    type: "input",
+                    name: "school"
+                }
+            ]).then(internRes => {
+                let intern = new Intern(param.role, res.name, res.id, res.email, internRes.school)
+                arr.push(intern);
+                console.log(arr)
+                start()
+            })
+        }
+        else if(param.role == "Manager") {
+            inquirer.prompt([
+                {
+                    message: "Enter team member's office number:",
+                    type: "input",
+                    name: "officenumber"
+                }
+            ]).then(managerRes => {
+                let manager = new Manager(param.role, res.name, res.id, res.email, managerRes.officenumber)
+                arr.push(manager);
+                console.log(arr)
+                start()
+            })
+        }
+    })
+}
+
+start()
--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+let top=`<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,16 +11,33 @@
 <header class = "bg-danger text-white text-center p-4"><h2>My Team</h2></header>
 
 <div class="container">
-        <div class="col-6">
-            <div class="card mx-auto" style="width: 18rem">
-                <h5 class="card-header">Example<br /><br />Engineer</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID</li>
-                    <li class="list-group-item">email</li>
-                    <li class="list-group-item">GitHub</li>
-                </ul>
-            </div>
-        </div>
-</div>
+`
+
+let bottom = `</div>
 </body>
 </html>
+`
+
+function htmlRender (arr) {
+    let middle = ``;
+
+    for(i=0;i< arr.length; i++){
+        middle += `<div class="col-6">
+            <div class="card mx-auto" style="width: 18rem">
+                <h5 class="card-header">Example<br /><br />${arr[i].name}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${arr[i].id}</li>
+                    <li class="list-group-item">${arr[i].email}</li>
+                    <li class="list-group-item">${arr[i].github}</li>
+                    <li class="list-group-item">${arr[i].officenumber}</li>
+                    <li class="list-group-item">${arr[i].school}</li>
+                </ul>
+            </div>
+    </div>
+        `
+    }
+
+    return top+middle+bottom;
+}
+
+module.exports = htmlRender
